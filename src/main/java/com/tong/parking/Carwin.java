f05b3cbc2efc1e068b6cf1821d10801540694616
icetong001 <doralee1005@gmail.com>
2021-06-01 15:05:07 +0800

    CARWIN

---
+++ src/main/java/com/tong/parking/Carwin.java
@@ -0,0 +1,16 @@
+package com.tong.parking;
+
+import javax.swing.*;
+
+public class Carwin {
+
+    static final int WIDTH=56;
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame();
+        frame.setSize(WIDTH,80);
+        frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
